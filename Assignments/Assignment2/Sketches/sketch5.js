5308b8d8e54a3928193f5042ff3d1aecb7ec6100
lucy3751 <78235983+lucy3751@users.noreply.github.com>
2021-06-16 10:26:54 -0400

    edit sketch 5

--- Assignments/Assignment2/Sketches/sketch5.js
+++ Assignments/Assignment2/Sketches/sketch5.js
@@ -1,55 +1,24 @@
-var canvasWidth = 1000;
-var canvasHeight = 1000;
+/*
+ UNINTUITIVE DRAWING - WITH LINES 
 
-/* 
-GET THE SPEED OF THE MOUSE MOVING - PART 1 - FIND TIME DIFFERENCE
-
-see the line of code about speed:
-https://p5js.org/reference/#/p5/winMouseX
-https://p5js.org/reference/#/p5/pwinMouseX
-
-
-https://p5js.org/reference/#/p5/pmouseX
-
-
-FIX THIS PART 
-var time;
-var time2;
-
-
-setTimeout(function () {
-    time2 = new Date();
-    console.log(time2 - time);
-}, 2000);
 */
-
-function mouseMoved() {
-    console.log("mouse moved");
-    return false; //this makes the code stop calling the function when the mouse is not moving
-}
-
 
 
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);
+    createCanvas(windowWidth, windowHeight);
     background('white');
 
     noFill();
-    rect(0, 0, canvasWidth, canvasHeight);
+    rect(0, 0, width, height);
 
 
-    frameRate(10);
+    frameRate(20);
 }
 
 function draw() {
-    var speed;
+    
 
-
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    speed = abs(mouseX - pmouseX);
-    // console.log(speed);
-
-    // console.log(time);
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
 
 
 }
